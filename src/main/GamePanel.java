f2a3729642ce10514cacc3421023c3a696f59846
Tofame <niewiadoma.65@gmail.com>
2023-11-21 10:37:34 +0100

    Game loop and WASD moving implemented
    Part 2 of RyiSnow's guide.

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,7 +1,10 @@
 package main;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JPanel;
 
@@ -17,22 +20,90 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
-    Thread gamThread;
+    // FPS
+    int FPS = 60;
+    int currentFPS = 0;
+
+    Font fpsFont = new Font("Arial", Font.PLAIN, 12);
+
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+
+    // Set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
-
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
-        gamThread = new Thread(this);
+        gameThread = new Thread(this);
         gameThread.start();
     }
 
     @Override
     public void run() {
-        
+        double drawInterval = 1000000000 / FPS; // ≈ 0.0167 seconds
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+
+        while (gameThread != null) {
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += currentTime - lastTime;
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                currentFPS++;
+            }
+
+            if (timer >= 1000000000) {
+                //System.out.println("FPS: " + currentFPS);
+                currentFPS = 0;
+                timer = 0;
+            }
+        }
+    }
+
+    public void update() {
+        if(keyH.upPressed == true) {
+            playerY -= playerSpeed;
+        } 
+        if(keyH.downPressed == true) {
+            playerY += playerSpeed;
+        }
+        if(keyH.leftPressed == true) {
+            playerX -= playerSpeed;
+        }
+        if(keyH.rightPressed == true) {
+            playerX += playerSpeed;
+        }
+    }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        // Draw player
+        g2.setColor(Color.white);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+        // Draw fps in left upper corner
+        //g2.setFont(fpsFont);
+        //g2.setColor(Color.white);
+        //g2.drawString("FPS: " + currentFPS, 10, 20);
+
+        g2.dispose();
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,6 @@ import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
@@ -17,5 +16,7 @@ public class Main {
 
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+
+        gamePanel.startGameThread();
     }
 }
